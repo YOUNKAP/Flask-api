5a4bab9cafc15fd34cfcbea8240b4506ae51e827
YOUNKAP NINA Duplex <nina.younkap@ccaa.pro>
2023-02-23 06:04:22 +0100

    CRUD complete

--- rp_flask_api/app.py
+++ rp_flask_api/app.py
@@ -1,30 +1,14 @@
-"""
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
-"""
-
-
-# app.py
-
-from flask import render_template # Remove: import Flask
 import connexion
+from flask import render_template
 
 app = connexion.App(__name__, specification_dir="./")
 app.add_api("swagger.yml")
 
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
--- rp_flask_api/people.py
+++ rp_flask_api/people.py
@@ -1,10 +1,11 @@
-# people.py
-
 from datetime import datetime
-from flask import abort
+
+from flask import abort, make_response
+
 
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+
 
 PEOPLE = {
     "Fairy": {
@@ -21,12 +22,12 @@ PEOPLE = {
         "fname": "Easter",
         "lname": "Bunny",
         "timestamp": get_timestamp(),
-    }
+    },
 }
+
 
 def read_all():
     return list(PEOPLE.values())
-
 
 
 def create(person):
@@ -41,7 +42,28 @@ def create(person):
         }
         return PEOPLE[lname], 201
     else:
-        abort(
-            406,
-            f"Person with last name {lname} already exists",
-        )
+        abort(406, f"Person with last name {lname} already exists")
+
+
+def read_one(lname):
+    if lname in PEOPLE:
+        return PEOPLE[lname]
+    else:
+        abort(404, f"Person with last name {lname} not found")
+
+
+def update(lname, person):
+    if lname in PEOPLE:
+        PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
+        PEOPLE[lname]["timestamp"] = get_timestamp()
+        return PEOPLE[lname]
+    else:
+        abort(404, f"Person with last name {lname} not found")
+
+
+def delete(lname):
+    if lname in PEOPLE:
+        del PEOPLE[lname]
+        return make_response(f"{lname} successfully deleted", 200)
+    else:
+        abort(404, f"Person with last name {lname} not found")
